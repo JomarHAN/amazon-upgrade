2d3726dab52e415dbddb5a9f02a3faccd4220db8
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-04-15 11:54:32 -0400

    connect socket in server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import productRouter from './routers/productRouters.js'
 import path from 'path'
 import uploadRouter from './routers/uploadRouter.js'
 import draftRouter from './routers/draftRouter.js'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 app.use(express.json())
@@ -46,6 +48,48 @@ app.get('/', (req, res) => {
 })
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
+})
+
+const httpServer = http.Server(app)
+const io = new Server(httpServer)
+const users = []
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        const user = users.find(user => user.socketId === socket.id)
+        if (user) {
+            user.online = false;
+            console.log('Offline', user.name)
+            const admin = users.find(user => user.isAdmin && user.online)
+            if (admin) {
+                io.to(admin.socketId).emit('updateUser', user)
+            }
+        }
+    })
+
+    socket.on('onLogin', (user) => {
+        const updateUser = {
+            ...user,
+            online: true,
+            socketId: socket.id,
+            messages: []
+        }
+        const existUser = users.find(x => x._id === updateUser._id)
+        if (existUser) {
+            existUser.online = true;
+            existUser.socketId = socket.id;
+        } else {
+            users.push(updateUser)
+        }
+        console.log('Online', user.name)
+        const admin = users.find(x => x.isAdmin && x.online)
+        if (admin) {
+            io.to(admin.socketId).emit('updateUser', updateUser)
+        }
+        if (updateUser.isAdmin) {
+            io.to(updateUser.socketId).emit('listUsers', users)
+        }
+    })
 })
 
 const port = process.env.PORT || 5000;
